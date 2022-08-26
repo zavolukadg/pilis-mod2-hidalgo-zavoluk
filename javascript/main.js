59a1c4c7390481d2bc1ad255abce6ca798697317
Zavoluk Anibal Dario <zavolukadg@gmail.com>
2022-08-26 19:47:22 -0300

    estructura completa

---
+++ javascript/main.js
@@ -0,0 +1,7 @@
+/* 1- ACCEDER A LOS ELEMENTOS */
+//GET ELEMENT BY ID
+/* let parrafo1 = document.getElementById('para1');
+console.log(parrafo1);
+// leer contenido
+console.log(parrafo1.textContent); */
+
