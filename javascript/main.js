b09f9e01bfe411b39ab9692e1ae52fac3577df1e
Zavoluk Anibal Dario <zavolukadg@gmail.com>
2022-08-27 00:40:59 -0300

    Ultimas Modificaciones

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,52 @@
+let contador = 1;
+
+function onClick(event) {
+    event.preventDefault();
+
+    const mensaje = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        telefono: document.getElementById('telefono').value,
+        tipo: document.getElementById('tipo').value
+    }
+    console.log(mensaje);
+
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify(mensaje),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+        .then((response) => response.json())
+        .then((json) => {
+            let resultado = parseInt(json.id) + contador;
+            contador++;
+            console.log(json);
+            Swal.fire({
+                title:'Bienvenido ' + json.name,
+                text:'Eres el participante n° ' + resultado,
+                item:'success',
+                timer: 4000
+                }
+            );
+            cleanForm();
+            /* redirectUrl(); */
+        })
+        .catch((err) => console.log(err));
+
+}
+
+function cleanForm() {
+    let formulario = document.getElementById('formulario');
+    formulario.reset();
+}
+function redirectUrl() {
+    window.location.href = "https://google.com";
+}
+
+let boton = document.getElementById("enviar");
+boton.addEventListener("click", onClick);
+
 function obtenerDatosClima() {
     /* fetch("//api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=5fcf4d95a836d3ef052d3a9dac50386c") */
     fetch("//api.openweathermap.org/data/2.5/weather?&lang=sp&units=metric&lat=-24.1834177&lon=-65.3334866&APPID=5fcf4d95a836d3ef052d3a9dac50386c")
